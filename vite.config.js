339e177cb036b94d4eec5972ee8118df183cca21
Jeigsaw <sumanull@outlook.com>
2023-01-13 23:28:20 +0545

    fixed webmanifest in vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,33 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+
+      manifest: {
+        "name": "ghadi",
+        "short_name": "ghadi",
+        "description": "A clock without numbers",
+        "id": "/ghadi/",
+        "scope": "/ghadi/",
+        "start_url": "/ghadi/",
+        "orientation": "portrait-primary",
+        "icons": [
+            {
+                "src": "/ghadi/android-chrome-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "/ghadi/android-chrome-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }
+        ],
+        "theme_color": "#1a2825",
+        "background_color": "#1a2825",
+        "display": "standalone"
       }
-  })
+    
+    })
   ]
 });
